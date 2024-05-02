d0b2fca26647dcee9cdf067989d4256f75541240
Jonathan de Montalembert <demonj92@gmail.com>
2024-05-02 11:32:43 +1000

    Swipe to delete on mobile

--- app/TodoItem.tsx
+++ app/TodoItem.tsx
@@ -2,39 +2,89 @@ import { Todo } from "./state/state";
 import { getTodoActions } from "@/state/reducer/todoReducer";
 import classNames from "classnames";
 import { isTouchScreen } from "@/helper/device";
+import SwipeableViews from "react-swipeable-views";
+import React, { useState } from "react";
 
 type TodoItemProps = {
   selected: boolean;
 } & Todo &
   ReturnType<typeof getTodoActions>;
+
 export const TodoItem = (todo: TodoItemProps) => {
   const isCompleted = todo.completedDate != null;
+  let [distance, setDistance] = useState(0);
+
   return (
     <li
       className={classNames(
-        "list-none",
+        "list-none relative",
         {
           "border-l-2 border-accent border-solid shadow-inner print:border-none":
             todo.selected && !isTouchScreen(),
         },
+        { "active:bg-accent/5": isTouchScreen() && distance < 0.2 },
+        { "active:bg-red-200": isTouchScreen() && distance >= 0.2 },
         {
           "border-l-2 border-transparent border-solid": !todo.selected,
         },
       )}
     >
-      <span className="flex gap-3 align-items-center py-2 lg:py-3  px-3 print:py-1">
-        <input
-          tabIndex={-1}
-          type="checkbox"
-          className="screen:checkbox"
-          checked={isCompleted}
-          onChange={() => todo.toggleCompletedTodo(todo.id)}
-        />
-        <label className="cursor-pointer flex-grow">
-          <span className="text-base">{todo.text}</span>
-        </label>
-        <span className={"text-sm "}>{todo.frequency}</span>
-      </span>
+      <SwipeableViews
+        resistance
+        onSwitching={(index, type) => {
+          if (type === "move") {
+            setDistance(index);
+          } else {
+            setDistance(0);
+          }
+          if (type === "end" && distance > 0.2) {
+            void todo.deleteTodo(todo.id);
+          }
+        }}
+      >
+        <span className="flex gap-3 align-items-center py-5 lg:py-3  px-3 print:py-1">
+          <input
+            tabIndex={-1}
+            type="checkbox"
+            className="screen:checkbox"
+            checked={isCompleted}
+            onChange={() => todo.toggleCompletedTodo(todo.id)}
+          />
+          <label className="cursor-pointer flex-grow">
+            <span className="text-base">{todo.text}</span>
+          </label>
+          <span className={"text-sm"}>{todo.frequency}</span>
+        </span>
+      </SwipeableViews>
+      <div
+        className={classNames(
+          "absolute right-4 top-0 bottom-0 flex items-center justify-center",
+        )}
+        style={{
+          opacity: distance < 0.2 ? distance : 1,
+        }}
+      >
+        <TrashCan />
+      </div>
     </li>
+  );
+};
+
+/*a trash can with a lid svg icon with only the outlines and being dark red as a component*/
+const TrashCan = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M3 6l2-2h14l2 2M6 6V21a2 2 0 002 2h8a2 2 0 002-2V6M10 11v6M14 11v6"></path>
+    </svg>
   );
 };
