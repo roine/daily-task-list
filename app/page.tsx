118640b58c9aba5ce09ca165756597019aeb3c17
Jonathan de Montalembert <demonj92@gmail.com>
2024-06-14 10:24:33 +1000

    Testing that SSE works in vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,32 @@
 "use client";
 import { TodolistTitle } from "@/ui/TodolistTitle";
 import Todolist from "@/ui/Todolist";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { TodoCreate } from "@/ui/TodoCreate";
 import { TodoFilter } from "@/ui/TodoFilter";
 
 export default function Page() {
+  const [messages, setMessages] = useState<string[]>([]);
+
   useEffect(() => {
     // if code exist in query param remove it
     if (window.location.search.includes("code")) {
       window.history.replaceState({}, document.title, "/");
     }
+  }, []);
+
+  useEffect(() => {
+    const eventSource = new EventSource("/api/stream");
+
+    eventSource.onmessage = function (event) {
+      console.log("received");
+      setMessages((prevMessages) => [...prevMessages, event.data]);
+    };
+
+    // Clean up the event source on component unmount
+    return () => {
+      eventSource.close();
+    };
   }, []);
 
   return (
